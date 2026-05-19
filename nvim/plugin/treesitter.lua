daaea01887b9c82789625b3ff015567969c29bb7
Hugo Lee <dev@proxies.foo>
2026-05-19 23:40:56 +0800

    refactor: map filetypes to treesitter parsers explicitly
    Signed-off-by: Hugo Lee <dev@proxies.foo>

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,21 +1,27 @@
+local treesitter = {
+	-- filetype(keys) = parser(values)
+	help = "vimdoc",
+	regex = "regex",
+	javascript = "javascript",
+	typescript = "typescript",
+	typescriptreact = "typescript",
+	python = "python",
+	lua = "lua",
+	json = "json",
+	bash = "bash",
+	sh = "bash",
+	css = "css",
+	html = "html",
+	astro = "astro",
+	swift = "swift",
+	yaml = "yaml",
+}
+
+require("nvim-treesitter").install(vim.tbl_values(treesitter))
+
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "css", "html", "javascript", "typescript", "tsx", "astro" },
-	callback = function()
-		vim.treesitter.start()
+	pattern = vim.tbl_keys(treesitter),
+	callback = function(args)
+		pcall(vim.treesitter.start, args.buf)
 	end,
-})
-require("nvim-treesitter").install({
-	"vimdoc",
-	"regex",
-	"javascript",
-	"typescript",
-	"python",
-	"lua",
-	"json",
-	"bash",
-	"css",
-	"html",
-	"tsx",
-	"astro",
-	"swift",
 })
