41281f2a8936d8408b822cca1bb21608d69f6b0d
Hugo Lee <dev@proxies.foo>
2026-05-27 04:50:35 +0800

    move options from settings to here
    Signed-off-by: Hugo Lee <dev@proxies.foo>

--- nvim/plugin/cmdline-auto-hidden.lua
+++ nvim/plugin/cmdline-auto-hidden.lua
@@ -1,6 +1,9 @@
 local api, o = vim.api, vim.o
 local cmdline_group = api.nvim_create_augroup("MinimalCmdLine", { clear = true })
 
+o.cmdheight = 0
+o.laststatus = 2
+
 api.nvim_create_autocmd("CmdlineEnter", {
 	group = cmdline_group,
 	pattern = "*",
